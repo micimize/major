3ac7083e1044cdd16ec9a827ce1129ff62491e27
micimize <rosenthalm93@gmail.com>
2020-04-22 11:54:40 -0500

    auth working?!?

--- todo_app/backend/@app/server/src/middleware/installFirebaseAuth.ts
+++ todo_app/backend/@app/server/src/middleware/installFirebaseAuth.ts
@@ -1,7 +1,9 @@
-import { auth } from "firebase-admin";
+import { auth, initializeApp } from "firebase-admin";
 import { Express } from "express";
+import { print } from "graphql";
 
-export default (app: Express) =>
+export default (app: Express) => (
+  initializeApp(),
   app.use(async (request: any, response: any, next: () => void) => {
     const authHeader = request.get("Authorization");
     const unauthorized = (body: string) => response.status(401).send(body);
@@ -10,13 +12,14 @@ export default (app: Express) =>
       return unauthorized("missing authorization header");
     }
 
-    const token = authHeader.replace(/^Bearer\s/, "");
+    const idToken = authHeader.replace(/^Bearer\s/, "");
 
     try {
-      request.firebaseUser = await auth().verifyIdToken(token);
+      request.firebaseUser = await auth().verifyIdToken(idToken);
       console.log(request.firebaseUser);
       next();
     } catch (err) {
       return unauthorized(err);
     }
-  });
+  })
+);
